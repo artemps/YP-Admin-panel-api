d2f64858cd8b46ef8d077dfe3a3fd8126f8643d5
Artem Pshenichnyi <fised@mail.ru>
2023-09-27 01:40:56 +0300

    добавляет настройки nginx, dockerfile, docker-compose

---
+++ docker_compose/simple_project/app/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Waiting for postgres... $POSTGRES_HOST $POSTGRES_PORT"
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  sleep 0.5
+done
+
+echo "PostgreSQL started"
+
+python3 manage.py collectstatic --no-input
+#python3 manage.py migrate
+
+uwsgi --ini uwsgi.ini
